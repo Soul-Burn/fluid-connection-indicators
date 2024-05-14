1ed2272d0596a95493f75a11681cb292af45e89b
Soul-Burn <sugoibaka@gmail.com>
2024-05-14 21:51:02 +0300

    Fixed indicators not disappearing on Transport Drones supply depots

--- settings.lua
+++ settings.lua
@@ -25,7 +25,7 @@ data:extend {
         type = "string-setting",
         name = "fci-ignored-entities",
         setting_type = "runtime-global",
-        default_value = "",
+        default_value = "supply-depot",
         allow_blank = true,
         auto_trim = true,
         order = "d",
