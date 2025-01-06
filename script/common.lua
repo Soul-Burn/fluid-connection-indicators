8ec086b8cc139aa81920ec441a531eb41af519cd
Soul-Burn <sugoibaka@gmail.com>
2025-01-06 19:39:36 +0200

    Support for ignoring new rails

--- script/common.lua
+++ script/common.lua
@@ -1,6 +1,10 @@
 local common = {}
 
-common.ignored_rail_neighbors = util.list_to_map { "straight-rail", "curved-rail", "locomotive", "cargo-wagon", "fluid-wagon", "artillery-wagon" }
+common.ignored_rail_neighbors = util.list_to_map {
+    "straight-rail", "curved-rail", "locomotive", "cargo-wagon", "fluid-wagon", "artillery-wagon",
+    "curved-rail-a", "curved-rail-b", "half-diagonal-rail",
+    "legacy-curved-rail", "legacy-straight-rail",
+}
 common.inserter_distance = 3
 
 return common
